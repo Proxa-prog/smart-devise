92e2c75166e7733cbe7dd51c499cda641939751d
Proxa-prog <84446082+Proxa-prog@users.noreply.github.com>
2022-05-08 16:47:55 +0400

    модальное окно, закрытие/открытие окна, валидация

---
+++ source/js/modules/modal-close.js
@@ -0,0 +1,17 @@
+const buttonClose = document.querySelector('.modal-feedback__button-close');
+const modalFeedback = document.querySelector('.modal-feedback');
+const isOpen = document.querySelector('.is-open');
+
+const modalClose = () => {
+  isOpen.addEventListener('click', (evt) => {
+    isOpen.style.display = 'none';
+    modalFeedback.style.display = 'none';
+  });
+
+  buttonClose.addEventListener('click', () =>{
+    isOpen.style.display = 'none';
+    modalFeedback.style.display = 'none';
+  });
+}
+
+export default modalClose;
